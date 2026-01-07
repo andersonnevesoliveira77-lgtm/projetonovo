7561391380c032e52417ba6904418c6481b28de2
andersonnevesoliveira77-lgtm <andersonnevesoliveira77@gmail.com>
2026-01-07 00:09:46 +0000

    index1

--- cuscuz_espacial.c
+++ cuscuz_espacial.c
@@ -2,12 +2,18 @@
 
 // Função responsavel por salvaros dados no arquivo
 void salvar (char tipo[], int agua, int tempo) {
+File  *arquivo;
+
+// Abre o arquivo em modo de acréscimo  (append)
+arquivo = fopen ("dados_cuscuz.txt","a");
+
+if (arquivo ==NULL) {
+    printf("Erro ao abrir o arquivo! \n");
+    return;
+}
 
 
 
 
 
-
-
-    
 }
