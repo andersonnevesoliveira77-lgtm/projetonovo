ca5cc125fab3bcf97589ead6efe79e4ea84a861f
andersonnevesoliveira77-lgtm <andersonnevesoliveira77@gmail.com>
2026-01-07 00:47:01 +0000

    index2

--- cuscuz_espacial.c
+++ cuscuz_espacial.c
@@ -12,8 +12,37 @@ if (arquivo ==NULL) {
     return;
 }
 
+// Escreve os dados no arquivo
+printf(arquivo,"Cuscuz: %s | Agua: %dml | tempo: %dmin\n", tipo, agua, tempo);
 
-
-
-
+// Feche o arquivo
+fclose(arquivo);
 }
+
+// Função principal que simula o preparoo
+void preparar_cuscuz() {
+    char tipo[50];
+    int agua;
+    int tempo;
+
+    printf("Digite o tipo de cuscuz: ");
+    scanf(" %[^\n]", tipo);
+
+    printf("Quantidade de agua (ml): ");
+    scanf("%d", &agua);
+
+    printf("Tempo de preparo (min): ");
+    scanf("%d", &tempo);
+
+    printf("\n Preparando cuscuz espacial...\n");
+
+    salvar(tipo, agua , tempo);
+
+    printf("cuscuz registrado com sucesso!\n");
+}
+
+int main() {
+    preparar_cuscuz();
+    retur 0;
+}
+
